b3dafd2b7157fee2e72f39821f92394a056206d5
Alamgir Ahamed <alamgir72435@gmail.com>
2023-12-20 20:54:28 +0600

    updates

---
+++ index.js
@@ -0,0 +1,21 @@
+// Importing the express module
+const express = require('express');
+
+// Creating an instance of the express application
+const app = express();
+
+// Define a route
+app.get('/', (req, res) => {
+  res.send('Hello, Express!');
+});
+
+// Define another route
+app.get('/api/greet', (req, res) => {
+  res.json({ message: 'Greetings from the Express API!' });
+});
+
+// Start the server on port 3000
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
